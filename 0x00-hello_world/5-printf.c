d3c7a231fd44b3f1cb292f711e49d48df64eec5b
Pompous-Coder <pompouscoder@gmail.com>
2022-09-08 08:07:57 -0700

    adding 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/**
+ * Printing more stuff
+ *  
+ */
+
+#include <stdio.h>
+
+/**
+ *
+ * main-contains code for output
+ * Return: 0 for sucees   
+ *    
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
