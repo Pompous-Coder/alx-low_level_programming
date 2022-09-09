d7becb71b93b77699d5cdda595f15e5d7ccf38a4
Pompous-Coder <pompouscoder@gmail.com>
2022-09-09 10:16:34 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main-Entry function
+ * Return: Always 0 if success
+ */
+int main(void)
+{
+	char str;
+	for (str = 'a'; str < 'z'; str++)
+		putchar(str);
+	putchar('\n');
+
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 0-either positive,negative or zero (printing)
 1-telling about last digit
+2-print alphabets
