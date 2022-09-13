7b61619120df59b8cc0f47c6e892b0099c7a222a
Pompous-Coder <pompouscoder@gmail.com>
2022-09-13 08:38:01 -0700

    meh

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main-Entry function
+ * Return: Always 0 if success
+ */
+void print_alphabet(void)
+{
+	char str;
+
+	for (str = 'a'; str <= 'z'; str++)
+		_putchar(str);
+
+	_putchar('\n');
+
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 1-using _putchar for printing
+1-printing lowercase letters
