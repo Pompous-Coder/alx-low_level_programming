93c4e5ee610e63044205880b026ac1dd1e30f732
Pompous-Coder <pompouscoder@gmail.com>
2022-09-13 15:58:39 -0700

    .

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * print_sign- Self explanatory function
+ * @n:tch
  * Description: Printing all lower case alphabets.
  * Return: Always 0 if success
  */
 int print_sign(int n)
-{
-	if (n > 0)
+{	if (n > 0)
 	{
 		_putchar ('+');
 		return (1);
