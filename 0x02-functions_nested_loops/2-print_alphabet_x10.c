ea237e8c314e9c61dfd6e347c3b5a454cc3d28b2
Pompous-Coder <pompouscoder@gmail.com>
2022-09-13 16:20:47 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,12 +5,15 @@
  * Return: Always 0 if success
  */
 void print_alphabet_x10(void)
-{	char str;
+{	
+	char str;
 	int cou;
 
 	for (cou = 0; cou <= 9; cou++)
-	{	for (str = 'a'; str <= 'z'; str++)
-		{	_putchar(str);
+	{	
+		for (str = 'a'; str <= 'z'; str++)
+		{
+			_putchar(str);
 		}
 
 		_putchar('\n');
