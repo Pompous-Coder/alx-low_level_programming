83f520d87f3e7342130179ef521c5cbced390a5e
Pompous-Coder <pompouscoder@gmail.com>
2022-09-13 14:04:32 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower- Self explanatory function
+ * Description: Printing all lower case alphabets.
+ * Return: Always 0 if success
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
