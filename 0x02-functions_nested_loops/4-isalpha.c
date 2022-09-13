6a948eaa7711a6c52acc695266db677cef51c61e
Pompous-Coder <pompouscoder@gmail.com>
2022-09-13 14:19:24 -0700

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,7 +4,7 @@
  * Description: Printing all lower case alphabets.
  * Return: Always 0 if success
  */
-int _islower(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
