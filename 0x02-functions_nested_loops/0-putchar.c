23fd0b32b4421ee182507bb39a66a92e0a58d5f3
Pompous-Coder <pompouscoder@gmail.com>
2022-09-13 07:10:43 -0700

    test

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main-Entry function
+ * Return: Always 0 if success
+ */
+int main(void)
+{
+	char str;
+
+	for (str = 'z'; str >= 'a'; str--)
+		putchar(str);
+	putchar('\n');
+
+	return (0);
+}
+1-printing _putchar using putchar
