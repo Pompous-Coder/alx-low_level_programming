fe346f173e2981a416e121cb7fa6f15e4eb6e088
Pompous-Coder <pompouscoder@gmail.com>
2022-09-13 15:14:47 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+i#include "main.h"
+/**
+ * print_last_digit- Self explanatory function
+ * Description: Printing all lower case alphabets.
+ * Return: Always 0 if success
+ */
+int print_last_digit(int a)
+{
+	int b;
+	b = (a % 10)
+	if (b <  0)
+		b* = (-1 * b;
+				)
+	_putchar(b + '0');
+	return (b);
+}
