adcb0f12a72f871ab231d523bd634be8e11fb940
Pompous-Coder <pompouscoder@gmail.com>
2022-09-13 14:41:36 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs- Self explanatory function
+ * Description: Printing all lower case alphabets.
+ * Return: Always 0 if success
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+		return (a);
+	else
+		return (a * -1);
+}
