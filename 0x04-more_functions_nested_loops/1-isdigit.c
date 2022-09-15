ef558de3ffeb50c9bfd4f507f2223d29d483a38b
Pompous-Coder <pompouscoder@gmail.com>
2022-09-15 09:16:51 -0700

    adding 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _isupper- check for uppercase
+ * _isdigit- check for uppercase
  * @c:a value to operate on
  * Description: checking for  uppercase
  * Return: Always 0 if success
  */
-int _isupper(int c)
+int _isdigit(int c)
 
 {
 	if (c >= '0'  && c <= '9')
