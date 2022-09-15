df1fa9658e7892140b165fc3fc88b595089bfe55
Pompous-Coder <pompouscoder@gmail.com>
2022-09-15 10:40:55 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers- Self explanatory function
+ * Description: Printing all numbers from 0-9.
+ * Return: Always 0 if success
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+		if (num == 2 || num == 4)
+			continue;
+		_putchar((num % 10) + '0');
+
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -2,3 +2,4 @@
 1-checking for numbers 0-9
 2-mul to numbers
 3-printing numbers from 0-9
+4-print 0-9 without 2 and 4
