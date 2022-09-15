0d5ccbe57d3e2186bb0352b7d68686991d7115d3
Pompous-Coder <pompouscoder@gmail.com>
2022-09-15 09:42:28 -0700

    adding 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers- Self explanatory function
+ * Description: Printing all numbers from 0-9.
+ * Return: Always 0 if success
+ */
+void print_numbers(void)
+{
+	int  num;
+
+	for (num = '0'; num <= '9'; num++)
+		_putchar(num);
+
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 1-checking for uppercase
 1-checking for numbers 0-9
 2-mul to numbers
+3-printing numbers from 0-9
