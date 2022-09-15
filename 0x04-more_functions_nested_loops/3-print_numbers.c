48b844b5500072131c6b9c99673ea7e52996503b
Pompous-Coder <pompouscoder@gmail.com>
2022-09-15 10:01:17 -0700

    adding 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,10 +6,10 @@
  */
 void print_numbers(void)
 {
-	char  num;
+	int  num;
 
-	for (num = '0'; num <= '9'; num++)
-		_putchar(num);
+	for (num = 0; num <= 9; num++)
+		_putchar(num + '0');
 
 	_putchar('\n');
 }
