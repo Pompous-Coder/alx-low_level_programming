46f78f5132daffe207ed49f21e134abc27703bec
Pompous-Coder <pompouscoder@gmail.com>
2022-09-19 06:16:23 -0700

    1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98- Self explanatory function
+ * @n:pointer
+ * Description: Printing numbers 0-14.
+ * Return: Always 0 if success
+ */
+void reset_to_98(int *n)
+{	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0-update int with pointer
