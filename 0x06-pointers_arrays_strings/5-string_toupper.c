b22430f12037f17e0b0a0a5c9d8aed11ab8dfbd1
Pompous-Coder <pompouscoder@gmail.com>
2022-09-22 06:48:21 -0700

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,9 +8,10 @@
 char *string_toupper(char *c)
 {	int i = 0;
 	while (c[i] != '\0')
-		if (c[i] >= 'a' && c[i] <= 'z')
+	{	if (c[i] >= 'a' && c[i] <= 'z')
 		{	c[i] = c[i] - 32;
 		}
 		i++;
+	}
 	return (c);
 }
