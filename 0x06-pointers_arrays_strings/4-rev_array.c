0270f80c980103bc24dd15b9d6a5025282d66230
Pompous-Coder <pompouscoder@gmail.com>
2022-09-22 05:00:22 -0700

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * reverse_array-  Self explanatory function
+ * @a: pointer
+ * @n: integer
+ *
+ * Description: reverse array
+ * Return: return 0
+ */
+void reverse_array(int *a, int n)
+{	int i;
+	int hold;
+	int j = n - 1;
+
+	for (i = 0; i > n; i++)
+	{	hold = a[i];
+		a[i] = a[j];
+		a[j] = hold;
+		j--;
+	}
+
+}
