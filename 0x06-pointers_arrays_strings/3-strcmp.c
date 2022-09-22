811185852b00ad0534bf94343ef3176e16f1c4c3
Pompous-Coder <pompouscoder@gmail.com>
2022-09-22 04:27:27 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,18 +1,19 @@
 #include "main.h"
 /**
- * _strncmp-  Self explanatory function
+ * _strcmp-  Self explanatory function
  * @s1: pointer
  * @s2: pointer
  *
  * Description: comparing strings.
  * Return: return compared results
  */
-int _strncmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {	int i = 0;
 
-	for  (; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while  (s1[i] != '\0' && s2[i] != '\0')
 		if (s1[i] != s2[i])
 		{	return (s1[i] - s2[i]);
 		}
+		i++;
 	return (0);
 }
