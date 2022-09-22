8011033537f50d0849fa5ba674348e0f95d7e69e
Pompous-Coder <pompouscoder@gmail.com>
2022-09-22 03:36:21 -0700

    2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncpy-  Self explanatory function
+ * @dest: pointer
+ * @src: pointer
+ * @n: number of bytes
+ *
+ * Description: Printing all numbers from 0-9.
+ * Return: Always 0 if success
+ */
+char *_strncpy(char *dest, char *src, int n)
+{	int sl = 0;
+	int dl = 0;
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	while (i < n)
+	{	dest[i] = '\0';
+		i++;	
+	}
+	return (dest);
+}
+
