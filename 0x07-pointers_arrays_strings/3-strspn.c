e5fa22c6bc1352c1547e88bbd24178a9b1f43232
Pompous-Coder <pompouscoder@gmail.com>
2022-09-27 04:46:45 -0700

    done

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strspn-  Self explanatory function
+ * @s: parameter
+ * @accept: parameter
+ *
+ * Description: performing task
+ * Return: return results
+ */
+unsigned int _strspn(char *s, char *accept)
+{	unsigned int i, j;
+	int c;
+
+	for (i = 0; s[i] != '0'; i++)
+	{	for (j = 0; accept[j] != '\0'; j++)
+		{	if (s[i] == accept[j])
+			{	c++;
+				break;
+			}
+		}
+	}
+	return (c);
+}
