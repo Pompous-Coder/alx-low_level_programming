7a22d5ff615ffbd89d304c32681df891ac1f4519
Pompous-Coder <pompouscoder@gmail.com>
2022-09-27 04:07:07 -0700

    doen

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,12 +8,12 @@
  * Return: return results
  */
 char *_strchr(char *s, char c)
-{	char findMe = 'c';
-	int i = 0;
+{	int i = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
-	{	if (s[i] == 'c')
-			return (&s[i]);
+	{	if (s[i] == c)
+		{	return (&s[i]);
+		}
 	}
 	return (NULL);
 }
