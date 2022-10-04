a07257649b62df9838550d398a0923df665ebd43
Pompous-Coder <pompouscoder@gmail.com>
2022-10-04 08:22:42 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-  Self explanatory function
+ * @argc: parameter
+ * @argv: parameter
+ *
+ * Description: performing task
+ * Return: return results
+ */
+int main(int argc, char *argv[])
+{	int bettyisfrustrating = 0;
+
+	if (argc <= 0)
+	{	return (-1);	
+	}
+	printf("%d\n", argc - 1);
+	return (0);
+}
