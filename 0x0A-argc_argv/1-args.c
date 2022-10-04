800daa617f17b0d32a3509291a87c7bc17fa164d
Pompous-Coder <pompouscoder@gmail.com>
2022-10-04 08:27:17 -0700

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,12 +7,10 @@
  * Description: performing task
  * Return: return results
  */
-int main(int argc, char *argv[])
-{	int bettyisfrustrating = 0;
-
-	if (argc <= 0)
-	{	return (-1);	
+int main(int argc, char *argv[]__attribute__((unused)))
+{	if (argc > 0)
+	{
+		printf("%d\n", (argc - 1));
 	}
-	printf("%d\n", argc - 1);
 	return (0);
 }
